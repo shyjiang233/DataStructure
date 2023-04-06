0132de1478ca8ba1a118d501e1bc6cbdc5778c2d
theshy <hello@qq.com>
2023-04-06 23:11:48 +0800

    dewdjgj

---
+++ src/main/java/回溯/分割/patition.java
@@ -0,0 +1,24 @@
+package 回溯.分割;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/*分割回文串*/
+public class patition {
+    List<List<String>> result=new ArrayList<>();
+    LinkedList<String> path=new LinkedList<>();
+   public  List<List<String>> partition(String s){
+       if(s.length()==1){
+           return  result;
+       }
+       backTracking(s,0);
+       return  result;
+   }
+
+    private void backTracking(String s, int startIndex) {
+
+       
+    }
+
+}
